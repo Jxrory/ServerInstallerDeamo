9fcf4232b1565e261ddc8ba90943d171fdd9bd34
Rory <rory@jxrory.com>
2026-08-01 21:10:28 +0800

    config nginx

--- DockerServices/server-hermes/nginx/config_nginx.sh
+++ DockerServices/server-hermes/nginx/config_nginx.sh
@@ -2,10 +2,10 @@
 
 SCRIPT_DIR="$(dirname "$0")"
 
-sudo cp ${SCRIPT_DIR}/nginx/hermes-gateway /etc/nginx/sites-available
+sudo cp ${SCRIPT_DIR}/hermes-gateway /etc/nginx/sites-available
 sudo ln -s /etc/nginx/sites-available/hermes-gateway /etc/nginx/sites-enabled/hermes-gateway
 
-sudo cp ${SCRIPT_DIR}/nginx/hermes-dashboard /etc/nginx/sites-available
+sudo cp ${SCRIPT_DIR}/hermes-dashboard /etc/nginx/sites-available
 sudo ln -s /etc/nginx/sites-available/hermes-dashboard /etc/nginx/sites-enabled/hermes-dashboard
 
 sudo nginx -t && sudo systemctl reload nginx
